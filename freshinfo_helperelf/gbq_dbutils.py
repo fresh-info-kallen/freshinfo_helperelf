334c9037adb191145360bea4bd6dafbabe9d4bde
Kallen Chan <kallen@freshinfo.co.nz>
2021-01-12 17:41:45 +1300

    Added create dataset function

--- freshinfo_helperelf/gbq_dbutils.py
+++ freshinfo_helperelf/gbq_dbutils.py
@@ -112,3 +112,23 @@ def upload_df_to_gbq_table(
         
     # Return Job Results 
     return this_result 
+
+
+
+def create_datset(client, dataset_name):
+
+    # TODO(developer): Set dataset_id to the ID of the dataset to create.
+    dataset_id = "{}.{}".format(client.project, dataset_name)
+
+    # Construct a full Dataset object to send to the API.
+    dataset = bigquery.Dataset(dataset_id)
+
+    # TODO(developer): Specify the geographic location where the dataset should reside.
+    dataset.location = "US"
+
+    # Send the dataset to the API for creation, with an explicit timeout.
+    # Raises google.api_core.exceptions.Conflict if the Dataset already
+    # exists within the project.
+    dataset = client.create_dataset(dataset, timeout=30)  # Make an API request.
+    print("Created dataset {}.{}".format(client.project, dataset.dataset_id))
+    
